c4395c182754f95598391e2908c085e59054032d
Arohan Dutt <arohandutt@gmail.com>
2020-12-14 22:49:00 -0500

    Setup webcam

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,23 @@
 <script>
+	import { onMount } from "svelte";
 
+	let videoEl;
+
+	onMount(async () => {
+		try {
+			const stream = await navigator.mediaDevices.getUserMedia({
+				video: true,
+				audio: false,
+				facingMode: "user",
+			});
+
+			videoEl.srcObject = stream;
+			videoEl.play();
+		} catch (e) {
+			console.error(e);
+		}
+	});
 </script>
-
-<main>
-
-</main>
 
 <style>
 	main {
@@ -14,5 +27,10 @@
 		margin: 0 auto;
 	}
 
-
+	video {
+		-webkit-transform: scaleX(-1);
+		transform: scaleX(-1);
+	}
 </style>
+
+<main><video bind:this={videoEl} width="600" height="480" /></main>
